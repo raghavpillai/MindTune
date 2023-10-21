56d105cb90348e8ba06437c78abacefa62c65d41
raghavpillai <raghavpillai101@gmail.com>
2023-10-20 23:55:09 -0400

    New sdk

--- server/v1/src/modules/tts_handler.py
+++ server/v1/src/modules/tts_handler.py
@@ -1,9 +1,36 @@
-
+import os
+from dotenv import load_dotenv
+from elevenlabs import generate, stream, set_api_key
+load_dotenv()
+set_api_key(os.getenv("ELEVENLABS_API_KEY"))
 
 
 class TTSHandler:
 
-    
+    @classmethod
+    def text_stream(cls):
+        text_to_stream: str = "This is an example of text that I'd like to stream. We will stream each individual word of the text."
+        words = text_to_stream.split()
+        len_word_context = 3
+        split_words = [words[i:i+len_word_context] for i in range(0, len(words), len_word_context)]
+        for sentence in split_words:
+            yield ' '.join(sentence)
+        
+        # for sentence in text_to_stream.split('.'):
+        #     yield sentence
+
+    @classmethod
+    def generate_audio(cls):
+        audio_stream: stream.Stream = generate(
+            text=cls.text_stream(),
+            voice="Nicole",
+            model="eleven_monolingual_v1",
+            stream=True
+        )
+        stream(audio_stream)
 
     def __init__(self) -> None:
         pass
+
+if __name__ == "__main__":
+    TTSHandler.generate_audio()
