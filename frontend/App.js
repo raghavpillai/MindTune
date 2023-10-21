a6504cf853796d656670be004ced709b50314657
raghavpillai <raghavpillai101@gmail.com>
2023-10-21 09:26:53 -0400

    New front page

--- frontend/App.js
+++ frontend/App.js
@@ -1,38 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { TamaguiProvider } from 'tamagui'
-import config from './tamagui.config'
-import { Button, XStack, YStack, Image} from 'tamagui'
-import logo from './assets/logo.png'
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useRef } from "react";
+import { StyleSheet, View, ImageBackground, Animated } from "react-native";
+import { TamaguiProvider } from "tamagui";
+import config from "./tamagui.config";
+import { Button, XStack, Image } from "tamagui";
 
 export default function App() {
+  const buttonsTranslateY = useRef(new Animated.Value(300)).current;
+  const backgroundTranslateY = useRef(new Animated.Value(500)).current;
+  const logoTranslateY = useRef(new Animated.Value(-300)).current;
+
+  useEffect(() => {
+    Animated.sequence([
+      // First, execute these two animations in parallel.
+      Animated.parallel([
+        Animated.timing(logoTranslateY, {
+          toValue: 0,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(backgroundTranslateY, {
+          toValue: 0,
+          duration: 2000,
+          useNativeDriver: true,
+        }),
+      ]),
+      // Once the parallel animations are done, execute this one.
+      Animated.timing(buttonsTranslateY, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
   return (
     <TamaguiProvider config={config}>
       <View style={styles.container}>
+        <Animated.View style={{ ...styles.backgroundImageContainer, transform: [{ translateY: backgroundTranslateY }] }}>
+          <ImageBackground
+            source={require("./assets/backdrop.png")}
+            resizeMode="cover"
+            style={styles.backgroundImage}
+          >
+            <Animated.View
+              style={{ ...styles.logoContainer, transform: [{ translateY: logoTranslateY }] }}
+            >
+              <Image
+                style={{
+                  resizeMode: "cover",
+                  height: 100,
+                  width: 350,
+                }}
+                source={require("./assets/logo.png")}
+              />
+            </Animated.View>
 
-      <YStack space width={"100%"} height={'50%'} alignItems='center'>
-        <XStack>
-          <Image
-            style={{
-              resizeMode: 'cover',
-              height: 100,
-              width: 350,
-            }}
-            source={require('./assets/logo.png')}
-          />
-        </XStack>
-        <YStack space width={'60%'} height={200} alignItems='center' justifyContent='flex-end'>
-          <XStack>
-            <Button width='100%' backgroundColor={'$blue9'} color={'white'}>Start Daily Log</Button>
-          </XStack>
-          <XStack></XStack>
-          <XStack>
-          <Button width='100%' backgroundColor={'$blue9'} color={'white'}>Access Dashboard</Button>
-          </XStack>
-        </YStack>
-      </YStack>
+            <Animated.View style={{ ...styles.buttonContainer, transform: [{ translateY: buttonsTranslateY }] }}>
+              <XStack style={styles.buttonSpacing}>
+                <Button width="75%" height={60} fontSize={20} backgroundColor={"white"} color={"$blue9"}>
+                  Check In
+                </Button>
+              </XStack>
+              <XStack>
+                <Button width="75%" height={60} fontSize={20} backgroundColor={"white"} color={"$blue9"}>
+                  See Your Progress
+                </Button>
+              </XStack>
+            </Animated.View>
+          </ImageBackground>
+        </Animated.View>
 
-      <StatusBar style="auto" />
+        <StatusBar style="auto" />
       </View>
     </TamaguiProvider>
   );
@@ -41,8 +80,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "flex-end",
+  },
+  backgroundImageContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+  },
+  logoContainer: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingBottom: 100,
+  },
+  buttonSpacing: {
+    marginBottom: 30,
   },
 });
