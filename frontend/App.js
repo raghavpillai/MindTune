7893fe0eed91cda9cdab78c6f74ffaea6953db57
Nikhil Narvekar <nikhil.narvekar.712@gmail.com>
2023-10-21 08:33:41 -0400

    tzamagui

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TamaguiProvider } from 'tamagui'
+import config from './tamagui.config'
+import { Button, XStack, YStack, Image} from 'tamagui'
+import logo from './assets/logo.png'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Yo yo alzhiemer</Text>
+    <TamaguiProvider config={config}>
+      <View style={styles.container}>
+
+      <YStack space width={"100%"} height={'50%'} alignItems='center'>
+        <XStack>
+          <Image
+            style={{
+              resizeMode: 'cover',
+              height: 100,
+              width: 350,
+            }}
+            source={require('./assets/logo.png')}
+          />
+        </XStack>
+        <YStack space width={'60%'} height={200} alignItems='center' justifyContent='flex-end'>
+          <XStack>
+            <Button width='100%' backgroundColor={'$blue9'} color={'white'}>Start Daily Log</Button>
+          </XStack>
+          <XStack></XStack>
+          <XStack>
+          <Button width='100%' backgroundColor={'$blue9'} color={'white'}>Access Dashboard</Button>
+          </XStack>
+        </YStack>
+      </YStack>
+
       <StatusBar style="auto" />
-    </View>
+      </View>
+    </TamaguiProvider>
   );
 }
 
